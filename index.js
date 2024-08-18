967fd702287517f14ae8087e83c11db69c8e3a85
Janniche Aarøen <jannicheaaroen@gmail.com>
2024-08-19 00:51:20 +0200

    Fjernet noen greier

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- Challenge: Add a button that, when clicked, gets a new deck of cards from the deckofcards API
- 
- URL: https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/
- 
- Log the whole response to the console
- */
-
 const getNewDeckBtnEl = document.getElementById("get-new-deck-btn");
 const drawBtnEl = document.getElementById("draw-btn");
 const computerEl = document.getElementById("computer");
